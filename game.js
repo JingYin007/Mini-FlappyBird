7df39d53e86fd65b5d3a4886659820b59d99a128
moTzxx <JingYin007@users.noreply.github.com>
2018-09-03 09:42:52 +0800

    ok

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
-var canvas = wx.createCanvas();
-console.log(canvas.width + '|' + canvas.height);
-var context = canvas.getContext('2d');
-context.fillStyle = 'red';
-context.fillRect(0, 0, 100, 100);
-=======
-import {Main} from "./Main.js";
+import { Main } from "./Main.js";
 new Main();
->>>>>>> d370af8c58e652b9975d8e674286713ce436f16b
+// var canvas = wx.createCanvas();
+// console.log(canvas.width + '|' + canvas.height);
+// var context = canvas.getContext('2d');
+// context.fillStyle = 'red';
+// context.fillRect(0, 0, 100, 100);
+
+
